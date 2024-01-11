0b52b87790649f38233057169a3ac4a0c87e2cd4
Jakub Stibůrek <jakub@stiburek.dev>
2024-01-11 13:27:33 +0100

    install harpoon, undotree, vim-fugitive

--- lua/jakub/packer.lua
+++ lua/jakub/packer.lua
@@ -16,4 +16,10 @@ return require('packer').startup(function(use)
 	use 'rebelot/kanagawa.nvim'
 
 	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+
+	use('theprimeagen/harpoon')
+
+	use('mbbill/undotree')
+
+	use('tpope/vim-fugitive')
 end)
